b135ab9895ca1639655ed5f02524c433645431fe
agan <1094143007@qq.com>
2023-06-21 23:55:57 +0800

    feat: 添加模版页面

---
+++ src/pages/about/[code].vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+defineOptions({ name: 'BusinessList' })
+definePageMeta({
+  layout: 'inner-page'
+})
+const { t } = useLang()
+useInnerPageContent({
+  name: t('about.name'),
+  slogan: t('about.name'),
+  topChannelCode: 'about',
+  pageChannelCode: 'culture'
+})!
+
+const { content, page, count } = useListByRouteCode()
+</script>
+
+<template>
+  <SiteList v-model:page="page" :data="content" :count="count" />
+</template>
+
+<style lang="scss">
+/** */
+</style>
